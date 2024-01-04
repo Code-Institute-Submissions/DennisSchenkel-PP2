b10b948c29053acdb911ac9da0bed2c6d6c3e2c7
Dennis Schenkel <dennis@Denniss-MBP.fritz.box>
2024-01-04 18:07:31 +0100

    JSON import test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 console.log("js integration works");
 
 // Import platforms.json
-import platforms from "../data/platforms.json" assert { type: 'json' };
+let platforms = import platforms from "../data/platforms.json" assert { type: 'json' };
 console.log(platforms);
+
+console.log("end of js integration");
