e6e24675155bd1c168c2c8138f471381b4caccd6
Dennis Schenkel <dennis@Denniss-MBP.fritz.box>
2024-01-06 19:03:01 +0100

    Add start page structure, basic css and function layout in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,93 @@
-console.log("js integration works");
+// Import professions.json
+import professions from "../data/professions.json" assert { type: 'json' };
 
 // Import platforms.json
-let platforms = import platforms from "../data/platforms.json" assert { type: 'json' };
-console.log(platforms);
+import platforms from "../data/platforms.json" assert { type: 'json' };
 
-console.log("end of js integration");
+console.log(platforms.platforms[0].platform);
+console.log(platforms.platforms[1].platform);
+console.log(platforms.platforms[2].platform);
+
+
+console.log(professions.professions[1]);
+
+
+
+// Functions to be used
+
+function processSteps() {
+
+};
+
+function navPreviousNext() {
+    // If on page one -> no previous
+
+    // If on page three -> results
+
+    // If on page four -> restart
+
+}
+
+
+// Start Page ------------------------------------
+
+
+
+
+// Step 1 ------------------------------------
+
+function loadProfessions() {
+
+} ;
+
+function selectorProfessions() {
+
+    // Was a value selected?
+
+
+} ;
+
+function loadSeniority() {
+
+} ;
+
+function selectorSeniority() {
+
+    // Was a value selected?
+
+} ;
+
+
+// Step 2 ------------------------------------
+
+
+function loadPlatforms() {
+
+};
+
+function selectorPlatforms() {
+
+    // Was at least one value selected?
+
+};
+
+
+// Step 3 ------------------------------------
+
+
+function validateBudget() {
+
+    // Validation if Budget is bigger then 200€ and smaller then 20.000€
+
+};
+
+
+
+// Step 4 ------------------------------------
+
+
+function generateResults() {
+
+};
+
+
