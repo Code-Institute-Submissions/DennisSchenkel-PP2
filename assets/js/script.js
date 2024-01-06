86e37ff96b3ed5ca42a3229a6870ce890c4121c4
Dennis Schenkel <dennis@Denniss-MBP.fritz.box>
2024-01-07 00:48:51 +0100

    Add logic for checked parameters on step 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,9 @@ console.log(platforms.platforms[2].platform);
 
 console.log(professions.professions[1]);
 
+// Parameters for calculation Arrey(Profession, Seniority, Array{Selectet Platforms}, Budget)
+let parameters = {};
+
 
 
 // Functions to be used
@@ -20,6 +23,8 @@ function processSteps() {
 };
 
 function navPreviousNext() {
+    
+    
     // If on page one -> no previous
 
     // If on page three -> results
@@ -27,6 +32,14 @@ function navPreviousNext() {
     // If on page four -> restart
 
 }
+
+// Step 1 Next Button
+
+let click = document.getElementById("next_btn_1");
+click.addEventListener("click", step1);
+
+
+
 
 
 // Start Page ------------------------------------
@@ -40,25 +53,42 @@ function loadProfessions() {
 
 } ;
 
-function selectorProfessions() {
+function step1(event) {
+    let selectedProfession = document.getElementById("profession_dropdown");
+    console.log(selectedProfession.value);
+    // Selected profession is added to parameters object
+    parameters.profession = selectedProfession.value;
 
-    // Was a value selected?
+    let senior = document.getElementById('senior_lvl');
+    let midlevel = document.getElementById('mid_lvl');
+    let junior = document.getElementById('junior_lvl');
+    let student = document.getElementById('student_lvl');
 
+    // Selected seniority is checked and added to parameters object
+    if (senior.checked) {
+        parameters.seniority = senior.value;
+    } else if (midlevel.checked) {
+        parameters.seniority = midlevel.value;
+    } else if (junior.checked) {
+        parameters.seniority = junior.value;
+    } else if(student.checked) {
+        parameters.seniority = student.value;
+    } else {
+        alert("Please select a seniority level you are looking for!");
+    }
 
-} ;
+    console.log(parameters);
 
-function loadSeniority() {
-
-} ;
-
-function selectorSeniority() {
-
-    // Was a value selected?
-
-} ;
+}
 
 
 // Step 2 ------------------------------------
+
+
+    // If logo of platform is selected the logo becomes colored. 
+    // In one object all platforms are included with value "false". 
+    // If selected, value changes to "true".
+    // With click on next platforms with "true" are added to parameters object.
 
 
 function loadPlatforms() {
