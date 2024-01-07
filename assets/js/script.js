2b8d37596cff6139d069e58c7a920a8d4de0dd98
Dennis Schenkel <dennis@Denniss-MBP.fritz.box>
2024-01-07 12:27:42 +0100

    Add navigation event listeners to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,10 +33,6 @@ function navPreviousNext() {
 
 }
 
-// Step 1 Next Button
-
-let click = document.getElementById("next_btn_1");
-click.addEventListener("click", step1);
 
 
 
@@ -45,20 +41,39 @@ click.addEventListener("click", step1);
 // Start Page ------------------------------------
 
 
+function loadStart(event) {
+    // Change HTML to start
+}
+
+    // Start Button
+let nextBtn0 = document.getElementById("next_btn_0");
+nextBtn0.addEventListener("click", loadStep1);
+
 
 
 // Step 1 ------------------------------------
 
+
+function loadStep1(event) {
+    // Load HTML of step1
+}
+
+    // Load Professions to dropdown
 function loadProfessions() {
 
 } ;
 
+    // Step 1 Logic
 function step1(event) {
+    
+    // Get selected profession
     let selectedProfession = document.getElementById("profession_dropdown");
     console.log(selectedProfession.value);
+
     // Selected profession is added to parameters object
     parameters.profession = selectedProfession.value;
 
+    // Get selected seniority
     let senior = document.getElementById('senior_lvl');
     let midlevel = document.getElementById('mid_lvl');
     let junior = document.getElementById('junior_lvl');
@@ -77,9 +92,17 @@ function step1(event) {
         alert("Please select a seniority level you are looking for!");
     }
 
+    // loadstep2(event)
     console.log(parameters);
 
 }
+
+    // Step 1 Next Button
+let nextBtn1 = document.getElementById("next_btn_1");
+nextBtn1.addEventListener("click", step1);
+
+
+
 
 
 // Step 2 ------------------------------------
@@ -91,33 +114,77 @@ function step1(event) {
     // With click on next platforms with "true" are added to parameters object.
 
 
-function loadPlatforms() {
 
-};
+function loadStep2(event) {
+    // Load HTML of step2
+}
 
-function selectorPlatforms() {
+    // Step 1 Logic
+function step2(event) {
 
-    // Was at least one value selected?
 
-};
+
+    // loadstep3(event)
+
+}
+
+    // Step 2 Next Button
+let nextBtn2 = document.getElementById("next_btn_2");
+nextBtn2.addEventListener("click", step2);
+    
+    // Step 2 Previous Button
+let previousBtn2 = document.getElementById("previous_btn_2");
+previousBtn2.addEventListener("click", loadStep1);
+    
+
+
 
 
 // Step 3 ------------------------------------
 
 
-function validateBudget() {
 
     // Validation if Budget is bigger then 200€ and smaller then 20.000€
 
-};
+function loadStep3(event) {
+    // Load HTML of step3
+}
+
+    // Step 1 Logic
+function step3(event) {
+
+    
+    // loadstep4(event)
+
+}
+
+    // Step 3 Next Button
+let nextBtn3 = document.getElementById("next_btn_3");
+nextBtn3.addEventListener("click", step3);
+    
+    // Step 3 Previous Button
+let previousBtn3 = document.getElementById("previous_btn_3");
+previousBtn3.addEventListener("click", loadStep2);
 
 
 
 // Step 4 ------------------------------------
 
 
-function generateResults() {
+function loadStep4(event) {
+    // Load HTML of step4
+}
 
-};
+    // Step 4 Logic (Calculate results)
+function step4(event) {
 
+    // Change HTML to start or previous
 
+}
+
+    // Step 4 Next Button - Restart - Goes back to index.html
+
+    // Step 4 Previous Button
+let previousBtn4 = document.getElementById("previous_btn_4");
+previousBtn4.addEventListener("click", loadStep3);
+    
