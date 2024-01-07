bcf5376eaa1fae2e8693aeb55a032a7c4bb6e3fb
Dennis Schenkel <dennis@Denniss-MBP.fritz.box>
2024-01-07 13:45:20 +0100

    Add HTML loading for step 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,16 +4,14 @@ import professions from "../data/professions.json" assert { type: 'json' };
 // Import platforms.json
 import platforms from "../data/platforms.json" assert { type: 'json' };
 
-console.log(platforms.platforms[0].platform);
-console.log(platforms.platforms[1].platform);
-console.log(platforms.platforms[2].platform);
+// console.log(platforms.platforms[0].platform);
 
+// console.log(professions.professions[1].profession);
 
-console.log(professions.professions[1]);
+// General ------------------------------------
 
 // Parameters for calculation Arrey(Profession, Seniority, Array{Selectet Platforms}, Budget)
 let parameters = {};
-
 
 
 // Functions to be used
@@ -22,28 +20,9 @@ function processSteps() {
 
 };
 
-function navPreviousNext() {
-    
-    
-    // If on page one -> no previous
-
-    // If on page three -> results
-
-    // If on page four -> restart
-
-}
-
-
-
-
 
 
 // Start Page ------------------------------------
-
-
-function loadStart(event) {
-    // Change HTML to start
-}
 
     // Start Button
 let nextBtn0 = document.getElementById("next_btn_0");
@@ -54,16 +33,28 @@ nextBtn0.addEventListener("click", loadStep1);
 // Step 1 ------------------------------------
 
 
-function loadStep1(event) {
-    // Load HTML of step1
-}
-
-    // Load Professions to dropdown
+/**
+ * Load Professions to dropdown
+ * Fills the professions dropdown with the elements defined in the professions.json.
+ * Professions are added to the dropdown by adding the html by using a for loop.
+ */
 function loadProfessions() {
 
-} ;
+    let professionDropdown = document.getElementById("profession_dropdown");
+    let newDropdownHTML = "";
+    
+    for (let i in professions.professions) {
+        newDropdownHTML += `<option value="${professions.professions[i].profession}">${professions.professions[i].profession}</option>`;
+    };
+    
+    professionDropdown.innerHTML = newDropdownHTML;
 
-    // Step 1 Logic
+};
+
+
+/**
+ * Load Step 1 Logic
+ */
 function step1(event) {
     
     // Get selected profession
@@ -90,18 +81,64 @@ function step1(event) {
         parameters.seniority = student.value;
     } else {
         alert("Please select a seniority level you are looking for!");
-    }
+    };
 
     // loadstep2(event)
     console.log(parameters);
 
-}
+};
 
-    // Step 1 Next Button
-let nextBtn1 = document.getElementById("next_btn_1");
-nextBtn1.addEventListener("click", step1);
+/**
+ * Load Step 1 Next Button
+ */
+function nextBtn1() {
+    let nextBtn1 = document.getElementById("next_btn_1");
+    nextBtn1.addEventListener("click", step1);
+};
 
+/**
+ * Loading HTML for step 1 (After everything else is loaded)
+ */
+function loadStep1(event) {
+    
+    let contentDiv = document.getElementById("content_div");
+    let newContentDivHTML = `
+        <form id="form_1">
+        <div class="form_element">
+            <label><h2>Select profession you are trying to reach?</h2></label><br>                        
+            <select id="profession_dropdown" name="profession">
+            <option value="Profession">Profession</option>
+            </select>
+        </div>
 
+        <div class="form_element">
+            <label><h2>What level of seniority are you looking for?</h2></label><br>
+            <input type="radio" class="seniority_radio" id="senior_lvl" name="seniority" value="Senior">
+            <label for="html">Senior</label><br>
+            <input type="radio" class="seniority_radio" id="mid_lvl" name="seniority" value="Midlevel">
+            <label for="css">Midlevel</label><br>
+            <input type="radio" class="seniority_radio" id="junior_lvl" name="seniority" value="Junior">
+            <label for="javascript">Junior</label><br>
+            <input type="radio" class="seniority_radio" id="student_lvl" name="seniority" value="Student">
+            <label for="javascript">Student</label><br>
+        </div>
+
+        <div class="nav_buttons">
+            <!-- Button to start page - No JS like the rest -->
+            <a href="index.html">
+                <button type="button" class="buttons" id="previous_btn_1">Previous</button>
+            </a>
+            <button type="button" class="buttons" id="next_btn_1">Next</button>
+        </div>
+        </form>
+    `
+    ;
+
+    contentDiv.innerHTML = newContentDivHTML;
+
+    loadProfessions();
+    nextBtn1();
+};
 
 
 
@@ -117,7 +154,7 @@ nextBtn1.addEventListener("click", step1);
 
 function loadStep2(event) {
     // Load HTML of step2
-}
+};
 
     // Step 1 Logic
 function step2(event) {
@@ -126,7 +163,7 @@ function step2(event) {
 
     // loadstep3(event)
 
-}
+};
 
     // Step 2 Next Button
 let nextBtn2 = document.getElementById("next_btn_2");
@@ -148,7 +185,7 @@ previousBtn2.addEventListener("click", loadStep1);
 
 function loadStep3(event) {
     // Load HTML of step3
-}
+};
 
     // Step 1 Logic
 function step3(event) {
@@ -156,7 +193,7 @@ function step3(event) {
     
     // loadstep4(event)
 
-}
+};
 
     // Step 3 Next Button
 let nextBtn3 = document.getElementById("next_btn_3");
@@ -173,14 +210,14 @@ previousBtn3.addEventListener("click", loadStep2);
 
 function loadStep4(event) {
     // Load HTML of step4
-}
+};
 
     // Step 4 Logic (Calculate results)
 function step4(event) {
 
     // Change HTML to start or previous
 
-}
+};
 
     // Step 4 Next Button - Restart - Goes back to index.html
 
